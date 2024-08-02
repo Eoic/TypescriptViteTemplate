e901c55f7de47cfec3294216e633e2f95a322726
Eoic <karolis.strazdas@protonmail.com>
2024-08-02 22:52:23 +0300

    Update main.ts.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,3 @@
 import '../styles/main.scss';
 
 console.log('It works!');
-
-let a = 10;
-
-if (a > 10) {
-    console.log("a is greater than 10");
-}
-
-if (a < 10) {
-    let b = 10;
-    console.log('a is less than 10');
-}
